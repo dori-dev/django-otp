2ce3070ed4eda6dc0a43d7b7516e2d73ae5bf25b
dori-dev <mr.dori.dev@gmail.com>
2023-02-28 20:00:23 +0330

    create custom user model

--- otp/models.py
+++ otp/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+from .manager import UserManager
+
+
+class MyUser(AbstractUser):
+    username = None
+    phone = models.CharField(
+        max_length=15,
+        unique=True,
+    )
+    otp = models.PositiveIntegerField(
+        blank=True,
+        null=True,
+    )
+    otp_create_time = models.DateTimeField(
+        auto_now=True,
+    )
+    objects = UserManager()
+    USERNAME_FIELD = 'phone'
+    REQUIRED_FIELDS = []
+    backend = ''
